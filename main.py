e6e60fe79ec35cb162282c720109e7afb6836161
Nikita <112660966+NikitaChemeris@users.noreply.github.com>
2024-10-31 19:19:55 +0100

    First running game

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+pygame.init()
+
+
+pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("My Pygame")
+
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            sys.exit()
