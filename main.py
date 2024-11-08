a37855f291b8b14a4ebe0dc33ed3e8bbc88ad9f6
Nikita <112660966+NikitaChemeris@users.noreply.github.com>
2024-11-08 09:58:12 +0100

    Continuous movement of the ship when the key is pressed

--- main.py
+++ main.py
@@ -9,20 +9,31 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Awesome Shooter Game")
 
+FIGHTER_STEP = 0.5
 fighter_image = pygame.image.load('images/fighter.png')
 fighter_width, fighter_height = fighter_image.get_size()
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
-FIGHTER_STEP = 20
+fighter_is_moving_left, fighter_is_moving_right = False, False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and fighter_x >= FIGHTER_STEP:
-                fighter_x -= FIGHTER_STEP
-            if event.key == pygame.K_RIGHT and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
-                fighter_x += FIGHTER_STEP
+            if event.key == pygame.K_LEFT:
+                fighter_is_moving_left = True
+            if event.key == pygame.K_RIGHT:
+                fighter_is_moving_right = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                fighter_is_moving_left = False
+            if event.key == pygame.K_RIGHT:
+                fighter_is_moving_right = False
+
+    if fighter_is_moving_left and fighter_x >= FIGHTER_STEP:
+        fighter_x -= FIGHTER_STEP
+    if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
+        fighter_x += FIGHTER_STEP
 
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
