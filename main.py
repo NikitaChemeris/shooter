3555d607cebc017d51e3f46828523284755dad74
Nikita <112660966+NikitaChemeris@users.noreply.github.com>
2024-11-08 10:31:31 +0100

    Moving and hiding the balloon

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ fighter_width, fighter_height = fighter_image.get_size()
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
 fighter_is_moving_left, fighter_is_moving_right = False, False
 
-rocket_image = pygame.image.load('images/rocket.png')
-rocket_width, rocket_height = rocket_image.get_size()
-rocket_x, rocket_y = fighter_x + fighter_width / 2 - rocket_width / 2, fighter_y - rocket_height
-rocked_was_fired = False
+BALL_STEP = 0.3
+ball_image = pygame.image.load('images/ball.png')
+ball_width, ball_height = ball_image.get_size()
+ball_x, ball_y = 0, 0
+ball_was_fired = False
 
 while True:
     for event in pygame.event.get():
@@ -30,7 +31,9 @@ while True:
             if event.key == pygame.K_RIGHT:
                 fighter_is_moving_right = True
             if event.key == pygame.K_SPACE:
-                rocked_was_fired = True
+                ball_was_fired = True
+                ball_x = fighter_x + fighter_width / 2 - ball_width / 2
+                ball_y = fighter_y - ball_height
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 fighter_is_moving_left = False
@@ -42,10 +45,16 @@ while True:
     if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
         fighter_x += FIGHTER_STEP
 
+    if ball_was_fired and ball_y + ball_height < 0:
+        ball_was_fired = False
+
+    if ball_was_fired:
+        ball_y -= BALL_STEP
+
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
 
-    if rocked_was_fired:
-        screen.blit(rocket_image, (rocket_x, rocket_y))
+    if ball_was_fired:
+        screen.blit(ball_image, (ball_x, ball_y))
 
     pygame.display.update()
