aaa6c4e293ce4ab9939dc1d8120c1c3c0a1f55ef
Nikita <112660966+NikitaChemeris@users.noreply.github.com>
2024-11-09 15:02:22 +0100

    Adding methods to the Ball class

--- ball.py
+++ ball.py
@@ -9,3 +9,24 @@ class Ball:
         self.step = BALL_STEP
         self.was_fired = False
         self.fighter = fighter
+
+    def fire(self):
+        self.was_fired = True
+        self.x = self.fighter.x + self.fighter.width / 2 - self.width / 2
+        self.y = self.fighter.y - self.height
+
+    def update_position(self):
+        if self.was_fired:
+            self.y -= self.step
+
+    def is_out_of_screen(self):
+        return self.y + self.height < 0
+
+    def reset(self):
+        self.was_fired = False
+
+    def is_collision(self, alien):
+        return (
+            alien.x < self.x < alien.x + alien.width - self.width and
+            alien.y < self.y < alien.y + alien.height - self.height
+        )
